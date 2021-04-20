a2e62a96081d595ae4a5a87ab1b81742dd07e1eb
temesgenmhr <tmsmhr@gmail.com>
2021-04-20 16:30:01 +0300

    Initial Commit of Infrastructute.Data, Update MVC for new UniversityDBContext

---
+++ DotNetCoreCleanArch/DotNetCoreCleanArch.Infrastructure.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,21 @@
+﻿using DotNetCoreCleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCoreCleanArch.Infrastructure.Data.Context
+{
+   public class UniversityDBContext : DbContext
+    {
+
+        public UniversityDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
--- DotNetCoreCleanArch/DotNetCoreCleanArch.Mvc/Startup.cs
+++ DotNetCoreCleanArch/DotNetCoreCleanArch.Mvc/Startup.cs
@@ -1,3 +1,4 @@
+using DotNetCoreCleanArch.Infrastructure.Data.Context;
 using DotNetCoreCleanArch.Mvc.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -35,6 +36,10 @@ namespace DotNetCoreCleanArch.Mvc
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+
+            services.AddDbContext<UniversityDBContext>(options =>
+              options.UseSqlServer(
+          Configuration.GetConnectionString("UniversityDatabaseConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
