16ac331aceedbc9ae2d57c3a84c3849912854e66
temesgenmhr <tmsmhr@gmail.com>
2021-04-20 17:17:52 +0300

    Commit Course Service, Repository and ViewModel

---
+++ DotNetCoreCleanArch/DotNetCoreCleanArch.Domain/Interfaces/ICourseRepository.cs
@@ -0,0 +1,14 @@
+﻿using DotNetCoreCleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCoreCleanArch.Domain.Interfaces
+{
+   public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+}
