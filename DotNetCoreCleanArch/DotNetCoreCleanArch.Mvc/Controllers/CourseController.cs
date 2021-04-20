cbcce31e11ea7aaa3b34dbb0870315512ea6b42f
temesgenmhr <tmsmhr@gmail.com>
2021-04-20 19:25:52 +0300

    Commit changes for presentation course controller

---
+++ DotNetCoreCleanArch/DotNetCoreCleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using DotNetCoreCleanArch.Application.Interfaces;
+using DotNetCoreCleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreCleanArch.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
